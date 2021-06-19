acb3c88b5095600edfcb61ba436d70f5c4969406
vuongvgc <vuongvgc@gmail.com>
2021-06-19 07:22:02 +0700

    Create component: pages and layouts, stepup index.js create store, create conect react redux, use redux thunk, use dev tool

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import reactDom from "react-dom";
+import rootReducers from "./reducers";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import App from "./App";
-reactDom.render(<App />, document.getElementById("root"));
+const enhance = compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+const store = createStore(rootReducers, enhance);
+
+reactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
