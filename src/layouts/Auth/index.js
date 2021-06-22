f4b3572e19043c9a9c4519187f609529b7792937
vuongvgc <vuongvgc@gmail.com>
2021-06-22 07:03:52 +0700

    reset style page and add variables

--- src/layouts/Auth/index.js
+++ src/layouts/Auth/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axiosClient from "../../utils/axiosClient";
 class AuthLayout extends React.Component {
   render() {
     return <div>{this.props.children}</div>;
--- src/layouts/Main/index.js
+++ src/layouts/Main/index.js
@@ -1,3 +1,11 @@
+import Header from "../Header";
+import Footer from "../Footer";
 export default function MainLayout(props) {
-  return <div>{props.children}</div>;
+  return (
+    <div>
+      <Header />
+      <div>{props.children}</div>
+      <Footer />
+    </div>
+  );
 }
