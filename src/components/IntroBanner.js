c68b455cb5f2c04ff18cb137f51ec856a383a560
vuongvgc <vuongvgc@gmail.com>
2021-06-29 06:56:18 +0700

    Create component IntroBanner and IntroBanner Item

---
+++ src/components/IntroBanner.js
@@ -0,0 +1,25 @@
+import IntroBannerItem from "./IntroBannerItem";
+function IntroBanner() {
+  return (
+    <section class="intro">
+      <div class="intro__content row justify-content-around">
+        <IntroBannerItem
+          icon="fab fa-codepen"
+          title="100,000 online courses"
+          content="Explore a variety of fresh topics"
+        />
+        <IntroBannerItem
+          icon="fa fa-asterisk"
+          title="Expert instruction"
+          content="Find the right instructor for you"
+        />
+        <IntroBannerItem
+          icon="fa fa-clock"
+          title="Lifetime access"
+          content="Learn on your schedule"
+        />
+      </div>
+    </section>
+  );
+}
+export default IntroBanner;
